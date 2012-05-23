48c6b6e7d700c4eea8bbdd2943de90d70288454d
Trever Fischer <tdfischer@fedoraproject.org>
2012-05-23 19:05:13 -0400

    Restructure things so there is a 'master' environment, which is the first loaded.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@
 
 export BUILDENV_HOME=${BUILDENV_HOME:-`dirname $BASH_SOURCE`}
 export BUILDENV_PREFIX=${BUILDENV_PREFIX:-/opt/buildenv}
+export BUILDENV_LOADED=""
 
 function _buildenv_set() {
   local _varname=$1
@@ -45,11 +46,19 @@ function _buildenv_restore_all() {
 
 function _buildenv_hook() {
   HOOK=$1
-  local _envname=${2:-$BUILDENV}
-  SCRIPT="$BUILDENV_HOME/$HOOK-env.d/$_envname.sh"
-  if [ -f "$SCRIPT" ];then
-    source "$SCRIPT"
+  for _envname in $BUILDENV_LOADED;do
+    SCRIPT="$BUILDENV_HOME/$HOOK-env.d/$_envname.sh"
+    if [ -f "$SCRIPT" ];then
+      source "$SCRIPT"
+    fi
+  done
+}
+
+function _buildenv_is_loaded() {
+  if [[ "$BUILDENV_LOADED" != "${BUILDENV_LOADED/ $_envname /}" ]];then
+    return 0
   fi
+  return 1
 }
 
 function _buildenv_load() {
@@ -58,10 +67,10 @@ function _buildenv_load() {
     return
   fi
   local _envname=$1
-  if [[ "$BUILDENV_LOADED" != "${BUILDENV_LOADED/ $_envname /}" ]];then
+  if _buildenv_is_loaded $_envname;then
     return
   fi
-  BUILDENV_PATH=${BUILDENV_PREFIX}/$_envname
+  local BUILDENV_PATH=${BUILDENV_PREFIX}/$_envname
   _pathvar="BUILDENV_PATH_"$(echo $_envname | sed -e s/-/_/)
   export $_pathvar="$BUILDENV_PATH"
   export BUILDENV_LOADED=" $_envname$BUILDENV_LOADED"
@@ -94,32 +103,6 @@ function _buildenv_autodetect() {
   basename `pwd`
 }
 
-function buildenv() {
-  local _envname=${1:-$(_buildenv_autodetect)}
-  if [ -z "$_envname" ];then
-    echo "Usage: buildenv package-name"
-    return 0
-  fi
-
-  if [ "$BUILDENV" == "$_envname" ];then
-    echo "You are already in $BUILDENV."
-    return 0
-  fi
-
-  if [ "$BUILDENV" != "$_envname" ];then
-    echo "Switching from ${BUILDENV:-nothing} to $_envname"
-    for f in $BUILDENV_LOADED;do
-      _buildenv_unload $f
-    done
-    _buildenv_restore_all
-  fi
-  export BUILDENV=$_envname
-  _buildenv_load $_envname
-  _buildenv_save PS1
-  export PS1="\[\e[1;32m\]$BUILDENV>\[\e[0m\]$PS1"
-  echo -e "Loaded environments: \E[1;33m$BUILDENV_LOADED\E[0m"
-}
-
 function _buildenv_complete() { 
   local cur prev environs
   COMPREPLY=()
@@ -139,3 +122,21 @@ function _buildenv_complete() {
 }
 
 complete -F _buildenv_complete buildenv
+
+function buildenv() {
+  local _envs_to_load=${@:-$(_buildenv_autodetect)}
+  if [ -z "$_envs_to_load" ];then
+    echo "Usage: buildenv package-name"
+    return 0
+  fi
+
+  for _envname in $_envs_to_load;do
+    if [ -z "$BUILDENV_MASTER" ];then
+      export BUILDENV_MASTER=$_envname
+    fi
+    _buildenv_load $_envname
+  done
+  export PROMPT_COMMAND='echo -en "\e[1;32m${BUILDENV_MASTER})\e[0m"'
+  echo -e "Loaded environments: \E[1;33m$BUILDENV_LOADED\E[0m"
+  echo -e "Master environment: \E[1;32m$BUILDENV_MASTER\E[0m"
+}
