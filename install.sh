a7ea8ce7c6e5889f7c8d62cda8ebe330f12a6f3e
Trever Fischer <tdfischer@fedoraproject.org>
2012-06-01 15:27:06 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
-source $(dirname $0)/setup.sh
+if [ -n "$BUILDENV_VERSION" ];then
+  echo "You already have $BUILDENV_VERSION installed at $BUILDENV_HOME."
+fi
+mydir=$(readlink -e $0)
+mydir=$(dirname $mydir)
+source $mydir/setup.sh
 
-echo "# Added by $(dirname $0)/install.sh" >> ~/.bashrc
+echo "# Added by $mydir/install.sh" >> ~/.bashrc
 echo "# Loads buildenv." >> ~/.bashrc
-echo "source $(dirname $0)/setup.sh" >> ~/.bashrc
+echo "if [ ! -f $mydir/setup.sh ];then" >> ~/.bashrc
+echo "  echo 'I tried loading buildenv from $mydir/setup.sh, but it could not be found.'" >> ~/.bashrc
+echo "else" >> ~/.bashrc
+echo "  source $mydir/setup.sh" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
 
 echo "Installed to ~/.bashrc."
 echo "For best results, execute the following:"
 echo "# mkdir -p $BUILDENV_PREFIX"
 echo "# chown $USER $BUILDENV_PREFIX"
 echo "This will allow you to run 'make install' without sudo."
+echo "To activate buildenv now, execute the following:"
+echo "$ source $mydir/setup.sh"
