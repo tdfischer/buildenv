3e32199c798826aab6bbd3a50203144bf17fce30
Trever Fischer <tdfischer@fedoraproject.org>
2012-08-24 10:30:33 -0400

    Display the buildenv_cwd_type in the prompt

--- init-ext.d/prompt.sh
+++ init-ext.d/prompt.sh
@@ -1,1 +1,6 @@
-export PS1="\[\033[1;32m\]\$BUILDENV_MASTER\[\033[0m\]$PS1"
+function _buildenv_dir_type() {
+  if [ -n "$BUILDENV_CWD_TYPE" ];then
+    echo "[$BUILDENV_CWD_TYPE]"
+  fi
+}
+export PS1="\[\033[1;32m\]\$BUILDENV_MASTER\$(_buildenv_dir_type)\[\033[0m\]$PS1"
