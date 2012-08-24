a0dc34da28481c718e3081a509c9c31e54247b76
Trever Fischer <tdfischer@fedoraproject.org>
2012-08-24 19:56:23 -0400

    Reset IFS hwne finished

--- lib/buildenv/libs.sh
+++ lib/buildenv/libs.sh
@@ -4,9 +4,11 @@ function _buildenv_lib_include() {
   IFS=":"
   for inc in $_buildenv_loaded;do
     if [ "$inc" == "$1" ];then
+      IFS=$_oifs
       return 0
     fi
   done
+  IFS=$_oifs
   source "$BUILDENV_HOME/lib/$1"
   _buildenv_loaded="$1:$_buildenv_loaded"
   return 0
