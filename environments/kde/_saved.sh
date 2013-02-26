029dc82255e39cd23e91f2a3241c52d98e60c85f
Trever Fischer <tdfischer@kde.org>
2013-02-26 09:41:15 -0500

    Add more environments

---
+++ environments/kde/_saved.sh
@@ -0,0 +1,4 @@
+# Saved on Tue Feb 19 14:54:09 EST 2013
+_buildenv_set GIT_AUTHOR_EMAIL "tdfischer@kde.org"
+# Saved on Tue Feb 19 14:54:41 EST 2013
+_buildenv_set GIT_COMMITTER_EMAIL "tdfischer@kde.org"
---
+++ environments/nebula/_saved.sh
@@ -0,0 +1,16 @@
+# Saved on Fri Feb 15 14:10:13 EST 2013
+_buildenv_set PKG_CONFIG_PATH "/opt/buildenv/nebula/lib/pkgconfig/"
+# Saved on Fri Feb 15 14:45:23 EST 2013
+_buildenv_set GIRDIR "/opt/buildenv/nebula/share/gir-1.0/"
+# Saved on Fri Feb 15 14:45:36 EST 2013
+_buildenv_set VALADIR "/opt/buildenv/nebula/share/vala/vapi/"
+# Saved on Fri Feb 15 14:46:03 EST 2013
+_buildenv_set VAPIDIR "/opt/buildenv/nebula/share/vala/vapi/"
+# Saved on Mon Feb 25 08:16:50 EST 2013
+_buildenv_set XDG_DATA_DIRS "/opt/buildenv/nebula/share/"
+# Saved on Mon Feb 25 08:17:05 EST 2013
+_buildenv_set VAPIDIR "/opt/buildenv/nebula/share/vala/vapi/"
+# Saved on Mon Feb 25 08:17:11 EST 2013
+_buildenv_set GIRDIR "/opt/buildenv/nebula/share/vala/vapi/"
+# Saved on Mon Feb 25 08:37:59 EST 2013
+_buildenv_set LD_LIBRARY_PATH "/opt/buildenv/nebula/lib"
