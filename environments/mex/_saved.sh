472e73383a08eadf0f42f96e928be27bd1a90843
Trever Fischer <tdfischer@fedoraproject.org>
2013-04-09 06:10:23 -0400

    Add mex

---
+++ environments/mex/_saved.sh
@@ -0,0 +1,8 @@
+# Saved on Wed Apr  3 12:15:31 EDT 2013
+_buildenv_set LD_LIBRARY_PATH "/opt/buildenv/mex/lib/"
+# Saved on Wed Apr  3 12:15:39 EDT 2013
+_buildenv_set PKG_CONFIG_PATH "/opt/buildenv/mex/lib/pkgconfig/"
+# Saved on Thu Apr  4 12:27:41 EDT 2013
+_buildenv_set XDG_DATA_DIRS "/home/tdfischer/Projects/collabora/mex/cogl/data/"
+# Saved on Thu Apr  4 12:31:16 EDT 2013
+_buildenv_set XDG_DATA_DIRS "/home/tdfischer/Projects/collabora/mex/media-explorer/data/"
