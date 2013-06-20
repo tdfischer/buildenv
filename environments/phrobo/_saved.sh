9a06dacbfbf7479fe7c5f8c6cc452ef7461d25ab
Torrie Fischer <wm161@wm161.net>
2013-06-20 02:30:32 -0400

    Cleanup phrobo env

--- environments/phrobo/_saved.sh
+++ environments/phrobo/_saved.sh
@@ -2,11 +2,3 @@
 _buildenv_set GIT_AUTHOR_EMAIL "tdfischer@phrobo.net"
 # Saved on Sat Feb  9 10:53:22 EST 2013
 _buildenv_set GIT_COMMITTER_EMAIL "tdfischer@phrobo.net"
-# Saved on Sat Feb  9 10:53:36 EST 2013
-_buildenv_set GIT_AUTHOR_NAME "Trever Fischer"
-# Saved on Sat Feb  9 10:53:42 EST 2013
-_buildenv_set GIT_COMMITTER_NAME "Trever Fischer"
-# Saved on Mon Feb 11 23:03:11 EST 2013
-_buildenv_set VIRTUAL_ENV_DISABLE_PROMPT "1"
-# Saved on Mon Mar 18 21:44:49 EDT 2013
-_buildenv_set RPM_PACKAGER "Trever Fischer <tdfischer@phrobo.net>"
