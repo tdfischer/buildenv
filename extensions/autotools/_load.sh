36972c2d3009268eb1dae7320655b420f6e23fe8
Torrie Fischer <tdfischer@kde.org>
2013-07-18 08:47:19 -0400

    add --libdir to ./configure

--- extensions/autotools/_load.sh
+++ extensions/autotools/_load.sh
@@ -1,7 +1,7 @@
 function configure_buildenv() {
   if [ ! -f ./configure -a -f ./autogen.sh ]; then
-    ./autogen.sh --prefix=$BUILDENV_PATH
+    ./autogen.sh --prefix=$BUILDENV_PATH --libdir=$BUILDENV_PATH/$(_buildenv_libdir)
   else
-    ./configure --prefix=$BUILDENV_PATH
+    ./configure --prefix=$BUILDENV_PATH --libdir=$BUILDENV_PATH/$(_buildenv_libdir)
   fi
 }
