f1243483dbd52757ef69449a32a0760f4530958b
Torrie Fischer <torrie@ripple.com>
2014-08-25 21:06:11 -0700

    fix quotes

--- extensions/makeenv/_load.sh
+++ extensions/makeenv/_load.sh
@@ -1,11 +1,11 @@
 _buildenv_lib_include buildenv/platform.sh
 
 function makeenv() {
-  if [ -n $BUILDENV_LOADED ];then
+  if [ -n "$BUILDENV_LOADED" ];then
     buildenv
   fi
   local makefile=$(_buildenv_environment_path Makefile)
-  if [ -f $makefile ];then
+  if [ -f "$makefile" ];then
     (
       mkdir -p $BUILDENV_PATH/makeenv
       cd $BUILDENV_PATH/makeenv
