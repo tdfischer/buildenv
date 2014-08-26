99ecde02603c9cc60c3cfa8e8c37dd77b57182c5
Torrie Fischer <torrie@ripple.com>
2014-08-25 21:13:01 -0700

    add some scons vars to rippled

---
+++ environments/rippled/_saved.sh
@@ -0,0 +1,4 @@
+# Saved on Thu Aug 14 09:59:43 PDT 2014
+_buildenv_set BOOST_ROOT "/opt/buildenv/boost1.55/makeenv/boost_1_55_0/"
+# Saved on Thu Aug 14 09:59:50 PDT 2014
+_buildenv_set OPENSSL_ROOT "/opt/buildenv/openssl/"
